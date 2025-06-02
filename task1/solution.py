2db755419a7fd612824799c877c1c31cbcd55b3d
elena_dobruk <elena.dobruk@yandex.ru>
2025-06-02 12:49:15 +0300

    update task1_solution, add docstrings

--- task1/solution.py
+++ task1/solution.py
@@ -3,13 +3,19 @@ import inspect
 
 
 def strict(func):
+    """A decorator to check if function's arguments are
+    in accordance with their annotations.
+    """
     @functools.wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args):
         sig = inspect.signature(func)
         for new_arg, protofunc_arg in zip(args, sig.parameters.values()):
             if isinstance(new_arg, protofunc_arg.annotation):
-                result = func(*args, **kwargs)
+                continue
             else:
-                raise TypeError
+                if protofunc_arg.annotation == 'inspect._empty':
+                    raise TypeError(f'No annotation provided for argument {new_arg}')
+                raise TypeError(f'Argument {new_arg}: wrong type, expected type {protofunc_arg.annotation}')
+        result = func(*args)
         return result
     return wrapper
